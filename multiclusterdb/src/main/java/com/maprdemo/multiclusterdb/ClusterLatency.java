e0bb049a7b0237baeb3d7bd510df07e5904b4293
edackram <edackram@gmail.com>
2017-05-07 14:08:15 -0700

    Modified to allow dynamic creation of TDigests

--- multiclusterdb/src/main/java/com/maprdemo/multiclusterdb/ClusterLatency.java
+++ multiclusterdb/src/main/java/com/maprdemo/multiclusterdb/ClusterLatency.java
@@ -1,5 +1,8 @@
 package com.maprdemo.multiclusterdb;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.tdunning.math.stats.ArrayDigest;
 import com.tdunning.math.stats.TDigest;
 
@@ -7,23 +10,21 @@ public class ClusterLatency {
 
 	String cluster;
 	long defaultSLA;
-	ArrayDigest getDigest = TDigest.createArrayDigest(100);
-	ArrayDigest putDigest = TDigest.createArrayDigest(100);
-	ArrayDigest scanDigest = TDigest.createArrayDigest(100);
+	Map<String, ArrayDigest> digests = new HashMap<String, ArrayDigest>();
 	
 	public ClusterLatency(String cluster, long startSLA) {
 		this.cluster = cluster;
 		this.defaultSLA = startSLA;
 	}
 	
-	public long getLatency() {
+	public long getLatency(String type) {
 		//Get 50% percentile if at least 10 entries
-		if (getDigest.size() > 10) {
-			double latency = getDigest.quantile(0.5d);
+		if (digests.get(type).size() > 10) {
+			double latency = digests.get(type).quantile(0.5d);
 			//Latency has to be at least 1 millisecond
 			if (latency < 1)
 				latency = 1d;
-			System.out.println("Get Latency for cluser " + cluster + " is: " + latency);
+			System.out.println(type + " Latency for cluser " + cluster + " is: " + latency);
 			return (long)latency; 
 		} else {
 			System.out.println("Using Default latency.......");
@@ -31,33 +32,13 @@ public class ClusterLatency {
 		}
 	}
 	
-	public long scanLatency() {
-		//Get 50% percentile if at least 10 entries
-		if (scanDigest.size() > 10) {
-			double latency = scanDigest.quantile(0.5d);
-			//Latency has to be at least 1 millisecond
-			if (latency < 1)
-				latency = 1d;
-			System.out.println("Get Latency for cluser " + cluster + " is: " + latency);
-			return (long)latency; 
-		} else {
-			System.out.println("Using Default latency.......");
-			return defaultSLA;
+	public void addLatency(String type, double latency) {
+		System.out.println("Adding " + type + " latency of: " + latency + " for cluster " + cluster);
+		if (digests.get(type) != null)	
+			digests.get(type).add(latency);
+		else {
+			digests.put(type, TDigest.createArrayDigest(100));
 		}
 	}
-	
-	public void addGetLatency(double latency) {
-		System.out.println("Adding get latency of: " + latency + " for cluster " + cluster);
-		getDigest.add(latency);
-	}
-	
-	public void addPutLatency(double latency) {
-		System.out.println("Adding put latency of: " + latency + " for cluster " + cluster);
-		putDigest.add(latency);
-	}
-	
-	public void addScanLatency(double latency) {
-		System.out.println("Adding scan latency of: " + latency + " for cluster " + cluster);
-		scanDigest.add(latency);
-	}
+
 }
